a64c6c9850846e367ee09a3ceec4a282f9bd501e
LeCyberDucky <19520383+LeCyberDucky@users.noreply.github.com>
2021-01-07 17:27:10 +0100

    Back to more manual version of script

--- Assignment 1/Implementation/mm_batch.sh
+++ Assignment 1/Implementation/mm_batch.sh
@@ -18,42 +18,45 @@
 # matmult_f.gcc
 #
 EXECUTABLE=matmult_c.gcc
-# LOGEXT=matmult_Ofast_funroll_loops_flto.dat
+LOGEXT=matmult_-O2.dat
 
 # define the mkn values in the MKN variable
 #
 # SIZES="100 200 500 1000 2000"
-SIZES="10 20 50 100"
+SIZES="10 20 50 100 200 500"
 
 # define the permutation type in PERM
 #
 PERMUTATIONS="nat mnk mkn nmk nkm kmn knm lib"
 # PERMUTATIONS="lib"
 
-OPTIMIZATIONLEVELS="-Ofast -O2 -O3"
+# OPTIMIZATIONLEVELS="-Ofast -O2 -O3"
+# Additional flag: mfma
 
 # uncomment and set a reasonable BLKSIZE for the blk version
 #
 # BLKSIZE=1
 
 # enable(1)/disable(0) result checking
-export MATMULT_COMPARE=0
+export MATMULT_COMPARE=1
 
 # start the collect command with the above settings
 
-LOGEXT=""
-
-for OPT in $OPTIMIZATIONLEVELS
-do
+# LOGEXT=""
+# MAKEOPTIONS=""
+# for OPT in $OPTIMIZATIONLEVELS
+# do
     # No further optimization
-    LOGEXT="matmult_"
-    LOGEXT+="${OPT}"
-    LOGEXT+="_.dat"
+    # LOGEXT="matmult_"
+    # LOGEXT+="${OPT}"
+    # LOGEXT+="_.dat"
     # LOGEXT="$OPT_matmult_.dat"
     /bin/rm -f $LOGEXT
 
-    make clean
-    make OPT=-g $OPT -std=c11
+    # make clean
+    # MAKEOPTIONS="OPT=-g ${OPT} -std=c11"
+    # make $MAKEOPTIONS
+    # make OPT=-g $OPT -std=c11
 
     for PERM in $PERMUTATIONS
     do
@@ -64,62 +67,68 @@ do
     done
     echo %$SIZES >> $LOGEXT
 
-    # funroll-loops
-    # LOGEXT="${OPT}_funroll_loops_matmult_.dat"
-    LOGEXT="matmult_"
-    LOGEXT+="${OPT}"
-    LOGEXT+="_funroll_loops_.dat"
-    /bin/rm -f $LOGEXT
+    # # funroll-loops
+    # # LOGEXT="${OPT}_funroll_loops_matmult_.dat"
+    # LOGEXT="matmult_"
+    # LOGEXT+="${OPT}"
+    # LOGEXT+="_funroll_loops_.dat"
+    # /bin/rm -f $LOGEXT
 
-    make clean
-    make OPT=-g -funroll_loops $OPT -std=c11
+    # make clean
+    # # make OPT=-g -funroll_loops $OPT -std=c11
+    # MAKEOPTIONS="OPT=-g -funroll_loops ${OPT} -std=c11"
+    # make $MAKEOPTIONS
 
-    for PERM in $PERMUTATIONS
-    do
-        for S in $SIZES
-        do
-            ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
-        done
-    done
-    echo %$SIZES >> $LOGEXT
+    # for PERM in $PERMUTATIONS
+    # do
+    #     for S in $SIZES
+    #     do
+    #         ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
+    #     done
+    # done
+    # echo %$SIZES >> $LOGEXT
 
-    # flto
-    # LOGEXT="${OPT}_flto_matmult_.dat"
-    LOGEXT="matmult_"
-    LOGEXT+="${OPT}"
-    LOGEXT+="_flto_.dat"
-    /bin/rm -f $LOGEXT
+    # # flto
+    # # LOGEXT="${OPT}_flto_matmult_.dat"
+    # LOGEXT="matmult_"
+    # LOGEXT+="${OPT}"
+    # LOGEXT+="_flto_.dat"
+    # /bin/rm -f $LOGEXT
 
-    make clean
-    make OPT=-g -flto $OPT -std=c11
+    # make clean
+    # # make OPT=-g -flto $OPT -std=c11
+    # MAKEOPTIONS="OPT=-g -flto ${OPT} -std=c11"
+    # make $MAKEOPTIONS
 
-    for PERM in $PERMUTATIONS
-    do
-        for S in $SIZES
-        do
-            ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
-        done
-    done
-    echo %$SIZES >> $LOGEXT
+    # for PERM in $PERMUTATIONS
+    # do
+    #     for S in $SIZES
+    #     do
+    #         ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
+    #     done
+    # done
+    # echo %$SIZES >> $LOGEXT
 
-    # funroll-loops and flto
-    # LOGEXT="${OPT}_funroll_loops_flto_matmult_.dat"
-    LOGEXT="matmult_"
-    LOGEXT+="${OPT}"
-    LOGEXT+="_funroll_loops_flto_.dat"
-    /bin/rm -f $LOGEXT
+    # # funroll-loops and flto
+    # # LOGEXT="${OPT}_funroll_loops_flto_matmult_.dat"
+    # LOGEXT="matmult_"
+    # LOGEXT+="${OPT}"
+    # LOGEXT+="_funroll_loops_flto_.dat"
+    # /bin/rm -f $LOGEXT
 
-    make clean
-    make OPT=-g -funroll_loops -flto $OPT -std=c11
+    # make clean
+    # # make OPT=-g -funroll_loops -flto $OPT -std=c11
+    # MAKEOPTIONS="OPT=-g -funroll_loops -flto ${OPT} -std=c11"
+    # make $MAKEOPTIONS
 
-    for PERM in $PERMUTATIONS
-    do
-        for S in $SIZES
-        do
-            ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
-        done
-    done
-    echo %$SIZES >> $LOGEXT
+    # for PERM in $PERMUTATIONS
+    # do
+    #     for S in $SIZES
+    #     do
+    #         ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
+    #     done
+    # done
+    # echo %$SIZES >> $LOGEXT
 
 
     # /bin/rm -f $LOGEXT
@@ -131,7 +140,7 @@ do
     #         ./$EXECUTABLE $PERM $S $S $S $BLKSIZE | grep -v CPU >> $LOGEXT
     #     done
     # done
-done
+# done
 
 
 # ./aos.${CC} $LOOPS $particles | grep -v CPU >> aos.$LOGEXT
