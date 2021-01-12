1471fd5d654a1a043a8c39820dcec658c8c0837f
LeCyberDucky <19520383+LeCyberDucky@users.noreply.github.com>
2021-01-12 21:03:56 +0100

    Attempt at jacobi and initialization functions. Still buggy.

---
+++ Assignment2/Implementation/init.c
@@ -0,0 +1,63 @@
+#include <math.h>
+
+// Function for initializing f, u, and u_next according to given initial conditions
+void init(double*** f, double*** u, double*** u_next, int edge_point_count, double edge_width) {
+    // f: Cube of function values -> Second derivatives of temperature
+    // u: Cube of temperature estimates of previous iteration
+    // u_next: Cube to hold new temperature estimates
+    // edge_point_count: Number of points along an axis
+    // edge_width: Physical width of an edge (-1..1 = 2)
+
+    int min_point = 0;
+    int max_point = edge_point_count - 1;
+
+    // Initialize u to 0 degrees everywhere except for the walls
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                // TODO: Not too sure about these boundary conditions
+                // Initializing the 5 walls to 20 degrees. Every other point becomes 0 degrees
+                if(
+                   y == max_point || x == max_point || x == min_point || z == min_point || z == max_point 
+                ) {
+                    u[x][y][z] = 20.0;
+                } else {
+                    u[x][y][z] = 0.0;
+                }
+            }
+        }
+    }
+
+    // Initialize u_next to 0.0
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                u_next[x][y][z] = 0.0;
+            }
+        }
+    }
+
+    // Initialize f (based on radiator)
+    int x_min = 0;
+    int x_max = (int)floor((-3.0/8.0 + 1.0)/edge_width * ((double)max_point)); // -1..1 = 0..2 => -1..-3/8 = 0..-3/8 + 8/8 = 0..5/8
+    int y_min = 0;
+    int y_max = (int)floor((-1.0/2.0 + 1.0)/edge_width * ((double)max_point));
+    int z_min = (int)ceil((-2.0/3.0 + 1.0)/edge_width * ((double)max_point));
+    int z_max = max_point;
+
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                f[x][y][z] = 0.0;
+                if (x_min <= x && x <= x_max) {
+                    if (y_min <= y && y <= y_max) {
+                        if (z <= z_min && z <= z_max) {
+                            f[x][y][z] = 200.0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- Assignment2/Implementation/jacobi.c
+++ Assignment2/Implementation/jacobi.c
@@ -3,7 +3,27 @@
  */
 #include <math.h>
 
-void
-jacobi() {
-    // fill in your code here
+
+// Expected type signature: 
+// int jacobi(double ***, double ***, double ***, int, int, double *);
+// TODO:Still unclear what all of this is -> figure that out. My guess: 
+// int jacobi(double*** f, double*** u, double*** u_next, int n, int ???, double* ???)
+
+// TODO:
+// Does not yet compile, as the type signature doesn't match
+// Also, the calculations are not correct yet
+void jacobi(double*** f, double*** u, double*** u_next, int edge_point_count, double delta) {
+    // f: Cube of function values -> Second derivatives of temperature
+    // u: Cube of temperature estimates of previous iteration
+    // u_next: Cube to hold new temperature estimates
+    // edge_point_count: Number of points along an axis
+    // delta: Distance between two neighbor points along an axis
+
+    for (int i = 1; i < edge_point_count - 1; i++) {
+        for (int j = 1; j < edge_point_count - 1; j++) {
+            for (int k = 1; k < edge_point_count - 1; k++) {
+                u_next[i][j][k] = 1.0/6.0 * (u[i-1][j][k] + u[i+1][j][k] + u[i][j-1][k] + u[i][j+1][k] + u[i][j][k-1] + u[i][j][k+1] + delta*delta * f[i][j][k]);
+            }
+        }
+    }
 }
