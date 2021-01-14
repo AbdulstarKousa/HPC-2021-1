eeaf2c7feff47df45f7fdf939c6ac3928787cb10
LeCyberDucky <19520383+LeCyberDucky@users.noreply.github.com>
2021-01-14 17:48:04 +0100

    Fixing script for sequential comparison

--- Assignment2/Implementation/sequential_comparison.sh
+++ Assignment2/Implementation/sequential_comparison.sh
@@ -5,7 +5,7 @@
 #BSUB -o sequential_comparison_%J.out
 #BSUB -q hpcintro
 #BSUB -n 1
-#BSUB -R"span[hosts=1]"
+#BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=2048]"
 #BSUB -W 59
 
@@ -27,71 +27,6 @@ do
     { ./$GAUSSSEIDEL $SIZE $ITER $TOLE $START_T $IMG; } |& grep -v CPU >>$LOG_GAUSSSEIDEL
 done
 
-# for T in $THREADS
-# do
-# 	for S in $SCHEDULE
-# 	do
-# 		{ OMP_SCHEDULE=$S OMP_NUM_THREADS=$T ./$EXECUTABLE $SIZE_N $ITER $TOLE $START_T $IMG; } |& grep -v CPU >>$LOGEXT
-# 		echo $T |  grep -v CPU >>$LOGEXT
-# 		echo $S |  grep -v CPU >>$LOGEXT
-		
-# 	done
-# done
+echo sizes $PROBLEM_SIZES iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOG_JACOBI
 
-echo size $SIZE_N iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOG_JACOBI
-
-echo size $SIZE_N iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOG_GAUSSSEIDEL
-
-
-
-
-
-
-
-
-
-# #!/bin/bash
-# # 02614 - High-Performance Computing, January 2018
-# #
-# # batch script to run matmult on a decidated server in the hpcintro
-# # queue
-# #
-# # Author: Bernd Dammann <bd@cc.dtu.dk>
-# #
-# #BSUB -J opmJ_batch
-# #BSUB -o opmJ_batch_%J.out
-# #BSUB -q hpcintro
-# #BSUB -n 12
-# #BSUB -R "span[hosts=1]"
-# #BSUB -R "rusage[mem=2048]"
-# #BSUB -W 15
-
-# EXECUTABLE=poisson_j
-
-# THREADS="12 8 4 2 1"
-
-# #SCHEDULE="static static,5 static,10 dynamic dynamic,5 dynamic,25 guided guided,5"
-# SCHEDULE="static,10 dynamic,10"
-
-# LOGEXT=../Results/datjacobOPMsinTest3.dat
-
-# SIZE_N="100"
-# ITER="10000"
-# TOLE="0.001"
-# START_T="0"
-# IMG="4"  #image disabled -> 0 
-
-# for T in $THREADS
-# do
-# 	for S in $SCHEDULE
-# 	do
-# 		{ OMP_SCHEDULE=$S OMP_NUM_THREADS=$T ./$EXECUTABLE $SIZE_N $ITER $TOLE $START_T $IMG; } |& grep -v CPU >>$LOGEXT
-# 		echo $T |  grep -v CPU >>$LOGEXT
-# 		echo $S |  grep -v CPU >>$LOGEXT
-		
-# 	done
-# done
-
-# echo size $SIZE_N iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOGEXT
-
-
+echo size $PROBLEM_SIZES iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOG_GAUSSSEIDEL
