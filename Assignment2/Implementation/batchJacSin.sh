355cccd792e4b458d5209c3b848cd34d559e2993
idabv <43176866+idabv@users.noreply.github.com>
2021-01-14 15:14:43 +0100

    batch changes

--- Assignment2/Implementation/batchJacSin.sh
+++ Assignment2/Implementation/batchJacSin.sh
@@ -22,14 +22,23 @@ SCHEDULE="static static,5 static,10 dynamic dynamic,5 dynamic,25 guided guided,5
 
 LOGEXT=../Results/datjacobOPMsinTest.dat
 
+SIZE_N="1000"
+ITER="10000"
+TOLE="0.001"
+START_T="0"
+IMG="4"  #image disabled -> 0 
+
 for T in $THREADS
 do
 	for S in $SCHEDULE
 	do
-		{ OMP_SCHEDULE=$S OMP_NUM_THREADS=$T ./$EXECUTABLE 5 5 5 5; } |& grep -v CPU >>$LOGEXT
+		{ OMP_SCHEDULE=$S OMP_NUM_THREADS=$T ./$EXECUTABLE $SIZE_N $ITER $TOLE $START_T $IMG; } |& grep -v CPU >>$LOGEXT
 		echo $T |  grep -v CPU >>$LOGEXT
 		echo $S |  grep -v CPU >>$LOGEXT
+		
 	done
 done
+
+echo size $SIZE_N iterations $ITER tolerance $TOLE initial guess $START_T  |  grep -v CPU >>$LOGEXT
 
 
