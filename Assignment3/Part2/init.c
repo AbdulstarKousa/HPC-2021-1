b1c0583c3610d57329cb19f34249b936b4dcfc43
idabv <43176866+idabv@users.noreply.github.com>
2021-01-20 10:45:48 +0100

    Add all files to Poisson

---
+++ Assignment3/Part2/init.c
@@ -0,0 +1,84 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function for initializing f, u, and u_next according to given initial conditions
+void init(double*** f, double*** u, double*** u_next, int N, double start_T) {
+    // f: Cube of function values -> Second derivatives of temperature
+    // u: Cube of temperature estimates of previous iteration
+    // u_next: Cube to hold new temperature estimates
+    // edge_point_count: Number of points along an axis -> N 
+
+    double edge_width = 2.0;  // edge_width: Physical width of an edge (-1..1 = 2)
+    int min_point = 0;
+    int edge_point_count = N + 2; 
+    int max_point = edge_point_count - 1;
+    
+    #pragma omp parrallel shared(u, u_next, f, start_T)
+    {   
+    // // Initialize u to 0 degrees everywhere except for the walls
+    #pragma omp for
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                // TODO: Not too sure about these boundary conditions
+                // Initializing the 5 walls to 20 degrees. Every other point becomes 0 degrees
+                if( y == max_point || x == max_point || x == min_point || z == min_point || z == max_point ) 
+                {
+                    u[x][y][z] = 20.0;
+                    u_next[x][y][z] = 20.0; 
+                }
+                else if (y == min_point )
+                {
+                    u[x][y][z] = 0.0;
+                    u_next[x][y][z] = 0.0; 
+                }
+                 else {
+                    u[x][y][z] = start_T;
+                    u_next[x][y][z] = start_T; 
+                }
+            }
+        }
+    }
+
+    int x_min = 0;
+    int x_max = (int)(floor(((double)(edge_point_count/2.0))*(5.0/8.0)));
+    int y_min = 0;
+    int y_max = (int)(floor(((double)(edge_point_count/2.0))*(1.0/2.0)));
+    int z_min = (int)(ceil(((double)(edge_point_count/2.0))*(1.0/3.0)));
+    int z_max = (int)floor((edge_point_count/2.0));
+
+
+    //printf("printing: %d \n", (int)(floor(((double)(edge_point_count/2.0))*(1.0/3.0))));
+    //printf("printing: [%d %d] [%d %d] [%d %d] \n", x_min,x_max,y_min,y_max,z_min,z_max);
+    #pragma omp for
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                f[x][y][z] = 0.0;
+                if (x_min <= x && x <= x_max) {
+                    if (y_min <= y && y <= y_max) {
+                        if (z_min <= z && z <= z_max) {
+                            f[x][y][z] = 200.0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    } //End parallel region
+
+    /*
+    for (int x = 0; x < edge_point_count; x++) {
+        for (int y = 0; y < edge_point_count; y++) {
+            for (int z = 0; z < edge_point_count; z++) {
+                printf("printing f: [%d %d %d] %.3f \n", x,y,z,f[x][y][z]);
+            }
+        }
+    }
+    */
+    
+
+
+
+}
---
+++ Assignment3/Part2/main.c
@@ -0,0 +1,156 @@
+/* main.c - Poisson problem in 3D
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "alloc3d.h"
+#include "print.h"
+#include "sin_test.h"
+#include "init.h"
+
+#ifdef _JACOBI
+#include "jacobi.h"
+#endif
+
+#ifdef _JACOBI_OMP
+#include "jacobi_omp.h"
+#endif
+
+#ifdef _GAUSS_SEIDEL
+#include "gauss_seidel.h"
+#endif
+
+#ifdef _GAUSS_SEIDEL_OMP
+#include "gauss_seidel_omp.h"
+#endif
+
+#define N_DEFAULT 100
+
+int
+main(int argc, char *argv[]) {
+
+    int 	N = N_DEFAULT;
+    int 	iter_max = 1000;
+    double	tolerance;
+    double	start_T;
+    int		output_type = 0;
+    char	*output_prefix = "poisson_res";
+    char    *output_ext    = "";
+    char	output_filename[FILENAME_MAX];
+    double 	***u = NULL;
+    double 	***f = NULL;
+    double 	***u_next = NULL;
+    int m;
+
+    /* get the paramters from the command line */
+    N         = atoi(argv[1]);	// grid size
+    iter_max  = atoi(argv[2]);  // max. no. of iterations
+    tolerance = atof(argv[3]);  // tolerance
+    start_T   = atof(argv[4]);  // start T for all inner grid points
+    if (argc == 6) {
+	output_type = atoi(argv[5]);  // ouput type
+    }
+
+    int N2 = N + 2; 
+    // allocate memory
+    if ( (u = d_malloc_3d(N2, N2, N2)) == NULL ) {
+        perror("array u: allocation failed");
+        exit(-1);
+    }
+    if ( (u_next = d_malloc_3d(N2, N2, N2)) == NULL ) {
+        perror("array u_next: allocation failed");
+        exit(-1);
+    }
+    if ( (f = d_malloc_3d(N2, N2, N2)) == NULL ) {
+        perror("array f: allocation failed");
+        exit(-1);
+    }
+
+    //Iniliazie matrices 
+    #ifdef _SIN_TEST
+    printf("Running sin_test \n");
+    sin_init(f, u, u_next, N);
+    #else
+    init(f, u, u_next, N, start_T);
+    #endif
+
+
+    #ifdef _JACOBI
+    printf("Running jacobi sequential\n");
+    double start = omp_get_wtime();
+    double norm_check = jacobi(f, u, u_next, N, tolerance, iter_max, &m); 
+    double end = omp_get_wtime();
+    printf("Wall time %f \n", (end-start) );
+    //printf("Number of iterations run: %d \n", p);
+    printf("Norm result from norm %e\n",norm_check);
+    printf("#Nr. iterations= %d\n",m);
+    #endif
+
+    #ifdef _JACOBI_OMP
+    printf("Running Jacobi OMP\n");
+    double start = omp_get_wtime();
+    double norm_check = jacobiOMP(f, u, u_next, N, tolerance, iter_max, &m); 
+    double end = omp_get_wtime();
+    printf("Wall time %f \n", (end-start) );
+    //printf("Number of iterations run: %d \n", p);
+    printf("Norm result from norm %e\n",norm_check);
+    printf("#Nr. iterations= %d\n",m);
+    #endif
+
+    #ifdef _GAUSS_SEIDEL
+    printf("Running Gauss sequential\n");
+    double start = omp_get_wtime();
+    double norm_check = gauss_seidel(f, u, N, tolerance, iter_max, &m); 
+    double end = omp_get_wtime();
+    printf("Wall time %f \n", (end-start) );
+    //printf("Number of iterations run: %d \n", p);
+    printf("Norm result from norm %e\n",norm_check);
+    printf("#Nr. iterations= %d\n",m);
+    #endif
+
+    #ifdef _GAUSS_SEIDEL_OMP
+    printf("Running Gauss OMP \n");
+    double start = omp_get_wtime();
+    double norm_check = gauss_seidelOMP(f, u, N, tolerance, iter_max, &m); 
+    double end = omp_get_wtime();
+    printf("Wall time %f \n", (end-start) );
+    //printf("Number of iterations run: %d \n", p);
+    printf("Norm result from norm %e\n",norm_check);
+    printf("#Nr. iterations= %d\n",m);
+    #endif
+
+    #ifdef _SIN_TEST
+    printf("Running sin_check \n");
+    printf("Results of sin_check = %lf\n",sin_check(u, N));
+    #endif  
+
+    // dump  results if wanted 
+    switch(output_type) {
+	case 0:
+	    // no output at all
+	    break;
+	case 3:
+	    output_ext = ".bin";
+	    sprintf(output_filename, "%s_%d%s", output_prefix, N+2, output_ext);
+	    fprintf(stderr, "Write binary dump to %s: ", output_filename);
+	    print_binary(output_filename, N+2, u);
+	    break;
+	case 4:
+	    output_ext = ".vtk";
+	    sprintf(output_filename, "%s_%d%s", output_prefix, N+2, output_ext);
+	    fprintf(stderr, "Write VTK file to %s: ", output_filename);
+	    print_vtk(output_filename, N+2, u);
+	    break;
+	default:
+	    fprintf(stderr, "Non-supported output type!\n");
+	    break;
+    }
+
+    // de-allocate memory
+    free(u);
+    free(u_next);
+    free(f);
+
+    return(0);
+}
