224c54819c5f0425b85966f513ac78862448d958
NumericalPie <s130377@student.dtu.dk>
2021-01-21 12:59:18 +0100

    Speed results for GPU1-4

--- Assignment3/matmult_Implementation/functions.h
+++ Assignment3/matmult_Implementation/functions.h
@@ -11,13 +11,13 @@
 
 void matmult_gpu2(int m,int n,int k,double *A,double *B,double *C);
 
-// void matmult_gpu3(int m,int n,int k,double *A,double *B,double *C);
+void matmult_gpu3(int m,int n,int k,double *A,double *B,double *C);
 
-// void matmult_gpu4(int m,int n,int k,double *A,double *B,double *C);
+void matmult_gpu4(int m,int n,int k,double *A,double *B,double *C);
 
 // void matmult_gpu5(int m,int n,int k,double *A,double *B,double *C);
 
-// void matmult_gpu6(int m,int n,int k,double *A,double *B,double *C);
+void matmult_gpu6(int m,int n,int k,double *A,double *B,double *C);
 
 void matmult_gpulib(int m,int n,int k,double *A,double *B,double *C);
 
