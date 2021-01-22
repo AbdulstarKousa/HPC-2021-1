dab1d04bbaf9bc188583277ef49f8b7fe239f7d0
NumericalPie <s130377@student.dtu.dk>
2021-01-22 10:46:23 +0100

    data collection

--- Assignment3/matmult_Implementation/Batch_mat.sh
+++ Assignment3/matmult_Implementation/Batch_mat.sh
@@ -9,7 +9,7 @@
 #BSUB -J mm_batch
 #BSUB -o ../jobfiles/mm_batch_%J.out
 #BSUB -q hpcintrogpu
-#BSUB -n 1
+#BSUB -n 16
 #BSUB -R "rusage[mem=2048]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 20
@@ -21,9 +21,9 @@ numactl --physcpubind=1
 
 EXECUTABLE=matmult_f.nvcc
 # SIZES="2048"
-SIZES="16 32 64 128 256 512 1024"
+# SIZES="16 32 64 128 256 512 1024"
 # SIZES="4096 8192"
-# SIZES="16 32 64 128 256 512 1024 2048"
+SIZES="16 32 64 128 256 512 1024 2048 4096 8192"
 
 
 PERMUTATIONS="lib"
@@ -33,8 +33,8 @@ for P in $PERMUTATIONS
 do
 	for S in $SIZES
 	do
-		LOGEXT=../matmult_Results/datmatmult_long_${P}.dat
-		MFLOPS_MIN_T=3 MFLOPS_MAX_IT=3 ./$EXECUTABLE $P $S $S*10 $S |& grep -v CPU >> $LOGEXT
+		LOGEXT=../matmult_Results/datmatmult_time2_${P}.dat
+		OMP_NUM_THREADS=16 MFLOPS_MIN_T=3 MFLOPS_MAX_IT=3 ./$EXECUTABLE $P $S $S*10 $S |& grep -v CPU >> $LOGEXT
 		echo permutation: $P size $S |  grep -v CPU >>$LOGEXT
 	done
 done
