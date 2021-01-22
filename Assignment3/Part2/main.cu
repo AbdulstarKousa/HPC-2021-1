89b30959267cfc3282486bb1be5f661fec042dc8
idabv <43176866+idabv@users.noreply.github.com>
2021-01-22 14:25:33 +0100

    ex7

--- Assignment3/Part2/main.cu
+++ Assignment3/Part2/main.cu
@@ -490,9 +490,11 @@ main(int argc, char *argv[]) {
     free(h_u);
     free(h_u_next);
     free(h_f);
-    free_gpu(d_u);
-    free_gpu(d_u_next);
-    free_gpu(d_f);
+    if(jacobi_type != 31 ){
+        free_gpu(d_u);
+        free_gpu(d_u_next);
+        free_gpu(d_f);
+    }
 
     return(0);
 }
