0b6d00048915f0cea518d9afcd1ed515686106ee
Karl <s130377@student.dtu.dk>
2021-01-22 18:13:14 +0100

    datahandling

--- Assignment3/matmult_Results/matmultPlotTimeEx3_32_16.py
+++ Assignment3/matmult_Results/matmultPlotTimeEx3_32_16.py
@@ -28,7 +28,7 @@ content = np.array(content)
 
 
 # Split data into runs
-content = np.split(content, len(content)/10)
+content = np.split(content, 9)
 print(content[0])
 data2 = pd.DataFrame(columns=["Problem Size[elm]", "Wall time[s]", "Permutation"])
 
@@ -42,20 +42,21 @@ for (i, run) in enumerate(content):
 with open("datmatmult_time_gpu5_16.dat") as f:
     content = f.readlines()
 
+
 content = [x.strip() for x in content]
 content = np.array(content)
 
-# Split data into runs
-content = np.split(content, len(content)/8)
 
-data = pd.DataFrame(columns=["Problem Size[elm]", "Kernel wall time[s]", "Memory wall time[s]", "Permutation"])
+# Split data into runs
+content = np.split(content, 10)
+print(content[0])
+data = pd.DataFrame(columns=["Problem Size[elm]", "Wall time[s]", "Permutation"])
 
 for (i, run) in enumerate(content):
     size = int(run[-1].split(' ')[-1])
-    Wtime = float(float(run[0].split(' ')[-1]) + float(run[2].split(' ')[-1]) + float(run[4].split(' ')[-1])) /3
-    Mtime = float(float(run[1].split(' ')[-1]) + float(run[3].split(' ')[-1]) + float(run[5].split(' ')[-1])) /3
+    Wtime = (float(run[0].split(' ')[-1]) + float(run[1].split(' ')[-1]) + float(run[2].split(' ')[-1])) /3
     Perm = str(run[-1].split(' ')[-3])
-    row = [size, Wtime, Mtime, Perm]
+    row = [size, Wtime, Perm]
     data.loc[i] = row
 
 
@@ -64,8 +65,8 @@ plot_names = ["Execution time vs Problem size", "Bla"]
 
 plt.figure(plot_names[0])
 plt.plot(lib["Problem Size[elm]"], lib["Wall time[s]"], label = lib["Permutation"][1], linestyle="-")
-plt.plot(data2["Problem Size[elm]"], data2["Wall time[s]"], label = data2["Permutation"][1], linestyle="-")
-plt.plot(data["Problem Size[elm]"], data["Kernel wall time[s]"] + data["Memory wall time[s]"], label = data["Permutation"][1], linestyle="--")
+plt.plot(data2["Problem Size[elm]"], data2["Wall time[s]"], label = "gpu5_32", linestyle="-")
+plt.plot(data["Problem Size[elm]"], data[ "Wall time[s]"], label = "gpu5_16", linestyle="--")
 plt.legend()
 plt.xlabel("Problem Size[elm]")
 plt.ylabel("Total wall time [s]")
