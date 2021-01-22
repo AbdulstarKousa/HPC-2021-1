964490fdef81c868b65dbc446291760a681829fd
idabv <43176866+idabv@users.noreply.github.com>
2021-01-22 15:00:55 +0100

    ex7

--- Assignment3/Part2/print.cu
+++ Assignment3/Part2/print.cu
@@ -1,8 +1,7 @@
 /* $Id: print.c,v 1.1 2019/12/12 15:03:38 gbarbd Exp gbarbd $ */
 #include <stdio.h>
 #include <inttypes.h>
-#include <cuda_runtime_api.h>
-#include <helper_cuda.h>
+//#include <cuda_runtime.h>
 
 static int
 is_little_endian(void) {
