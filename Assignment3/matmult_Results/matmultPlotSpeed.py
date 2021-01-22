f5381c4a97a7077a0476d571783824df4e2d912a
Karl <s130377@student.dtu.dk>
2021-01-22 19:34:47 +0100

    datahandling

--- Assignment3/matmult_Results/matmultPlotSpeed.py
+++ Assignment3/matmult_Results/matmultPlotSpeed.py
@@ -57,7 +57,7 @@ for (i, run) in enumerate(content):
     row = [size, Speed, Perm]
     data4.loc[i] = row
 
-with open("datmatmult_gpu5.dat") as f:
+with open("datmatmult_gpu6_32.dat") as f:
     content = f.readlines()
 
 content = [x.strip() for x in content]
@@ -68,6 +68,7 @@ content = np.split(content, len(content)/2)
 
 data5 = pd.DataFrame(columns=["Problem Size[elm]", "Speed", "Permutation"])
 
+print(content[0])
 for (i, run) in enumerate(content):
     size = int(run[-1].split(' ')[-1])
     Speed = float(run[0].split(' ')[1])
@@ -93,12 +94,30 @@ for (i, run) in enumerate(content):
     row = [size, Speed, Perm]
     datalib.loc[i] = row
 
+with open("datmatmult_lib.dat") as f:
+    content = f.readlines()
+
+content = [x.strip() for x in content]
+content = np.array(content)
+
+# Split data into runs
+content = np.split(content, len(content)/2)
+
+datalib = pd.DataFrame(columns=["Problem Size[elm]", "Speed", "Permutation"])
+
+for (i, run) in enumerate(content):
+    size = int(run[-1].split(' ')[-1])
+    Speed = float(run[0].split(' ')[1])
+    Perm = str(run[-1].split(' ')[-3])
+    row = [size, Speed, Perm]
+    datalib.loc[i] = row
+
 plot_names = ["Execution time vs Problem size", "Bla"]
 
 plt.figure(plot_names[0])
 plt.plot(data2["Problem Size[elm]"], data2["Speed"], label = data2["Permutation"][1], linestyle=":")
 plt.plot(data4["Problem Size[elm]"], data4["Speed"], label = data4["Permutation"][1], linestyle="-")
-plt.plot(data5["Problem Size[elm]"], data5["Speed"], label = data5["Permutation"][1], linestyle="-.")
+plt.plot(data5["Problem Size[elm]"], data5["Speed"], label = "gpu5_32", linestyle="-.")
 plt.plot(datalib["Problem Size[elm]"], datalib["Speed"], label = datalib["Permutation"][1], linestyle="--")
 plt.legend()
 plt.xlabel("Problem Size[elm]")
