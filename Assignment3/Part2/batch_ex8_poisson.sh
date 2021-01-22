55b620d34010e6800c0fc4a203d9cb6a284203b2
idabv <43176866+idabv@users.noreply.github.com>
2021-01-22 16:51:32 +0100

    ex7 data

--- Assignment3/Part2/batch_ex8_poisson.sh
+++ Assignment3/Part2/batch_ex8_poisson.sh
@@ -6,7 +6,7 @@
 #BSUB -R "rusage[mem=2048]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 20
-#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -gpu "num=1:mode=exclusive_process"
 
 module load cuda/11.1
 module load gcc/9.2.0
@@ -14,14 +14,14 @@ numactl --cpunodebind=1
 
 EXECUTABLE=poisson_j
 
-SIZE_N="10 20 30 40 50 60 100 150 200 250 300 400 500"
-ITER="100"
+SIZE_N="30 40 50 60 100 150"
+ITER="100000"
 TOLE="0.001"
 START_T="0"
 IMG="0"  #image disabled -> 0 
-EXERCISE11="31"
+EXERCISE11="41"
 
-LOGEXT0=Results_ex7/Exercise7_GPU_${ITER}.dat
+LOGEXT0=Results_ex8/Exercise8_GPU_${ITER}.dat
 for S in $SIZE_N
 do
 		{ ./$EXECUTABLE $S $ITER $TOLE $START_T $EXERCISE11; } |& grep -v CPU >>$LOGEXT0
